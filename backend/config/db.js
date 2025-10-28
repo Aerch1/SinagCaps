cc24a8ced7be176e796357dbdbd5740f7ae86699
Archie <archieval.llamas.2025@gmail.com>
2025-10-28 15:02:04 +0800

    da

--- backend/config/db.js
+++ backend/config/db.js
@@ -404,7 +404,7 @@ async function ensureSchema(conn) {
   // await conn.execute(`DROP TABLE IF EXISTS document_requests;`);
 
   await conn.execute(`
-  CREATE TABLE document_requests (
+CREATE TABLE IF NOT EXISTS document_requests (
    id INT AUTO_INCREMENT PRIMARY KEY,
   request_code VARCHAR(50) NOT NULL UNIQUE,
   user_id INT NULL,
